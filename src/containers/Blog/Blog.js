d46f9047e72bec83271bb6b310ea3e5ffb7a6275
Mark <youremail@gmail.comg>
2020-10-29 19:52:37 -0600

    restructured files to put full post, new post and posts into containers

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,59 +1,25 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import { Route } from "react-router-dom";
+
+import Posts from "./Posts/Posts"
 import './Blog.css';
-import axios from "../../axios";
 
 class Blog extends Component {
 
-    state = {
-        posts: [],
-        selectedPostId: null
-    }
-
-    componentDidMount () {
-        axios.get("/posts")
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: "Mark"
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log( response );
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id}); 
-    }
-
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-                />;
-        });
+
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
                             <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">Home</a></li>
+                            <li><a href="/new-post">New Posts</a></li>
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+            <Route path="/" render={() => <h1>Home</h1>}/>
             </div>
         );
     }
