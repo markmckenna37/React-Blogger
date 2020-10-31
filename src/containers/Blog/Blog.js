bfcb9159bc8eddd7e2a432efff53e759ae13c326
Mark <youremail@gmail.comg>
2020-10-31 10:45:26 -0600

    added NavLinks to blog.js, color and underline styling

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 
 import Posts from "./Posts/Posts"
 import './Blog.css';
@@ -15,13 +15,24 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Posts</a></li>
+                            <li><NavLink 
+                                    to="/" 
+                                    exact
+                                    activeClassName="my-active"
+                                    activeStyle={{
+                                        color: "#fa923f",
+                                        textDecoration: "underline"
+                                    }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: "/new-post",
+                                hash: "#submit",
+                                search: "?quick-submit=true"
+                            }}>New Posts</NavLink></li>
                         </ul>
                     </nav>
                 </header>
             <Route path="/" exact component={Posts} />
-            <Route path="/new-post" exact component={NewPost} />
+            <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
